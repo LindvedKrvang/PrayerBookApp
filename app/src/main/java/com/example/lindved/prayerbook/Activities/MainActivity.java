0030de20dc9097479fe4eae922e28b1601ed64a7
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2017-11-06 16:38:55 +0100

    Created a rough menu for the app and made it possible to go to prayersActivity

--- app/src/main/java/com/example/lindved/prayerbook/Activities/MainActivity.java
+++ app/src/main/java/com/example/lindved/prayerbook/Activities/MainActivity.java
@@ -3,16 +3,40 @@ package com.example.lindved.prayerbook.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.lindved.prayerbook.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGetAllPrayers;
+    Button btnCreateNewPrayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeButtons();
+        setOnClickListeners();
+    }
+
+    private void initializeButtons() {
+        btnGetAllPrayers = findViewById(R.id.btnGetAllPrayers);
+        btnCreateNewPrayer = (Button) findViewById(R.id.btnCreateNewPrayer);
+    }
+
+    private void setOnClickListeners(){
+        btnGetAllPrayers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToPrayersActivity();
+            }
+        });
+    }
+
+    private void goToPrayersActivity(){
         Intent intent = new Intent(this, PrayersActivity.class);
         startActivity(intent);
     }
